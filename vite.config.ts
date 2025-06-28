47c999325bf9bef6241ab5343961d194f3cdfac1
GuXian <2818958193@qq.com>
2025-06-28 12:09:42 +0800

    feat: 添加构建hash配置 - 为vite构建输出添加hash后缀 - 配置rollup输出文件名格式

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [Tov()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name].[hash].js`,
+				chunkFileNames: `[name].[hash].js`,
+				assetFileNames: `[name].[hash].[ext]`
+			}
+		}
+	}
 })
